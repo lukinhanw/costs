99b77afc40e6128b3d1ab584009a27417f4a67c5
lukinhanw <lukas.nw@hotmail.com>
2022-11-22 21:53:07 -0300

    aula25

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -7,6 +7,7 @@ import Button from '../form/Button'
 
 function NewProject() {
     const [categories, setCategories] = useState([])
+    
 
     useEffect(() => {
         fetch("http://localhost:5000/categories", {
